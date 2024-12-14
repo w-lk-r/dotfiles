8737737ac1b014b0bedee9bbcf5a1cc1351f5445
Jonathan Walker <w@lker.dev>
2024-12-14 16:50:39 +0900

    add neovim and other minor updates

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,65 @@
+-- always set leader first!
+vim.keymap.set("n", "<Space>", "<Nop>", { silent = true })
+vim.g.mapleader = " "
+
+-------------------------------------------------------------------------------
+--
+-- preferences
+--
+-------------------------------------------------------------------------------
+-- never ever folding
+vim.opt.foldenable = false
+vim.opt.foldmethod = 'manual'
+vim.opt.foldlevelstart = 99
+-- very basic "continue indent" mode (autoindent) is always on in neovim
+-- could try smartindent/cindent, but meh.
+-- vim.opt.cindent = true
+-- XXX
+-- vim.opt.cmdheight = 2
+-- vim.opt.completeopt = 'menuone,noinsert,noselect'
+-- not setting updatedtime because I use K to manually trigger hover effects
+-- and lowering it also changes how frequently files are written to swap.
+-- vim.opt.updatetime = 300
+-- if key combos seem to be "lagging"
+-- http://stackoverflow.com/questions/2158516/delay-before-o-opens-a-new-line
+-- vim.opt.timeoutlen = 300
+-- keep more context on screen while scrolling
+vim.opt.scrolloff = 2
+-- never show me line breaks if they're not there
+vim.opt.wrap = false
+-- always draw sign column. prevents buffer moving when adding/deleting sign
+vim.opt.signcolumn = 'yes'
+-- sweet sweet relative line numbers
+vim.opt.relativenumber = true
+-- and show the absolute line number for the current line
+vim.opt.number = true
+-- keep current content top + left when splitting
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+-- infinite undo!
+-- NOTE: ends up in ~/.local/state/nvim/undo/
+vim.opt.undofile = true
+--" Decent wildmenu
+-- in completion, when there is more than one match,
+-- list all matches, and only complete to longest common match
+vim.opt.wildmode = 'list:longest'
+-- when opening a file with a command (like :e),
+-- don't suggest files like there:
+vim.opt.wildignore = '.hg,.svn,*~,*.png,*.jpg,*.gif,*.min.js,*.swp,*.o,vendor,dist,_site'
+-- tabs: go big or go home
+vim.opt.shiftwidth = 8
+vim.opt.softtabstop = 8
+vim.opt.tabstop = 8
+vim.opt.expandtab = false
+-- case-insensitive search/replace
+vim.opt.ignorecase = true
+-- unless uppercase in search term
+vim.opt.smartcase = true
+-- never ever make my terminal beep
+vim.opt.vb = true
+-- show a column at 80 characters as a guide for long lines
+vim.opt.colorcolumn = '80'
+-- show more hidden characters
+-- also, show tabs nicer
+vim.opt.listchars = 'tab:^ ,nbsp:¬,extends:»,precedes:«,trail:•'
+
